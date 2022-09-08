024d64c1d144ec910a89e8e5a3fa4478e23c40b2
NachoHotz <juanhotz611@gmail.com>
2022-09-08 18:08:44 -0300

    FIX: remove middleware

--- routes/tasks.routes.go
+++ routes/tasks.routes.go
@@ -3,11 +3,10 @@ package routes
 import (
 	"github.com/labstack/echo"
 	"github.com/nachohotz/go-echo-restapi/controllers"
-	"github.com/nachohotz/go-echo-restapi/middlewares"
 )
 
 func TaskRouter(e *echo.Echo) {
-  tasksRouter := e.Group("/tasks", middlewares.BasicMiddleware)
+  tasksRouter := e.Group("/tasks")
 
   tasksRouter.GET("", controllers.GetTasks)
   tasksRouter.GET("/:id", controllers.GetTask)
